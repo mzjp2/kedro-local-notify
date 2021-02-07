fce9833d65a68f5ce17a132065971ecdb1cea414
Zain Patel <zain.patel06@gmail.com>
2021-02-07 18:12:09 +0000

    Add package metadata for PyPi (#2)

--- kedro_local_notify/__init__.py
+++ kedro_local_notify/__init__.py
@@ -1,3 +1,3 @@
 """kedro-local-notify"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- tests/test_kedro_notify.py
+++ tests/test_kedro_notify.py
@@ -2,4 +2,4 @@ from kedro_local_notify import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
